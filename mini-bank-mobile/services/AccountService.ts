294e2b6f548f6c6567a07be766b895ffcc237c4e
EdwardDiazR <edwardadriandiaz2020@gmail.com>
2025-05-29 12:54:54 -0400

    creating account service

---
+++ mini-bank-mobile/services/AccountService.ts
@@ -0,0 +1,22 @@
+export const TransferMoneyBetweenMyAccounts = (
+  amount: number,
+  originAccountId: number,
+  beneficiaryAccountId: number,
+  description: string
+) => {
+  //TODO: Logic to transfer money
+};
+
+export const TransferMoneyToBeneficiary = (
+  amount: number,
+  originAccountId: number,
+  beneficiaryProductId: number,
+  description: string
+) => {
+  //TODO: Logic to transfer money
+};
+
+export const PayServiceFromAccount = () => {};
+
+export const PayCreditCardFromAccount = () => {};
+export const PayLoanFromAccount = () => {};
